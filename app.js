98d9d9d99a28b2adb9a35b58df4e0a173f090124
krishansingh1 <kkrishan7074@gmail.com>
2022-04-09 13:43:41 +0530

    Commit

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const prev_btn=document.querySelector('.prev-btn');
-const next_btn=document.querySelector('.next-btn');
-const random_btn=document.querySelector('random-btn');
+const prev_btn = document.querySelector('.prev-btn');
+const next_btn = document.querySelector('.next-btn');
+const random_btn = document.querySelector('random-btn');
+const img = document.getElementById('person-img');
+const _name = document.getElementById('author');
+const job = document.getElementById('job');
+const review = document.getElementById('info');
 
+prev_btn.addEventListener('click', () => {
+    
+})
