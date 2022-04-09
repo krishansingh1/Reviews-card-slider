4618b4303fa1b2f9e3e86ee55576927f5bd0d1b5
krishansingh1 <kkrishan7074@gmail.com>
2022-04-09 13:49:58 +0530

    Commit

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'Susan Smith',
+        job: 'Web Developer',
+        img: '/media/person-1_rfzshl.jpg',
+        info: 'I\'m baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry'
+    },
+    {
+        id: 2,
+        name:'Anna Johnson',
+        job:'Web designer',
+        img:'/media/person-2_np9x5l.jpg',
+    }
+]
+
 const prev_btn = document.querySelector('.prev-btn');
 const next_btn = document.querySelector('.next-btn');
 const random_btn = document.querySelector('random-btn');
@@ -7,5 +23,5 @@ const job = document.getElementById('job');
 const review = document.getElementById('info');
 
 prev_btn.addEventListener('click', () => {
-    
+
 })
