cd05ec8ae372223de33db7b841eb45018c467868
krishansingh1 <kkrishan7074@gmail.com>
2022-04-09 14:03:04 +0530

    Commit

--- app.js
+++ app.js
@@ -6,22 +6,53 @@ const reviews = [
         img: '/media/person-1_rfzshl.jpg',
         info: 'I\'m baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry'
     },
+
     {
         id: 2,
-        name:'Anna Johnson',
-        job:'Web designer',
-        img:'/media/person-2_np9x5l.jpg',
+        name: 'Anna Johnson',
+        job: 'Web designer',
+        img: '/media/person-2_np9x5l.jpg',
+        info: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.'
+    },
+
+    {
+        id: 3,
+        name: 'Peter jones',
+        job: 'Intern',
+        img: '/media/person-3_ipa0mj.jpg',
+        info: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.'
+    },
+
+    {
+        id: 4,
+        name: 'Bill anderson',
+        job: 'The Boss',
+        img: '/media/person-4_t9nxjt.jpg',
+        info: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.'
     }
 ]
 
-const prev_btn = document.querySelector('.prev-btn');
-const next_btn = document.querySelector('.next-btn');
-const random_btn = document.querySelector('random-btn');
 const img = document.getElementById('person-img');
 const _name = document.getElementById('author');
 const job = document.getElementById('job');
 const review = document.getElementById('info');
 
+const prev_btn = document.querySelector('.prev-btn');
+const next_btn = document.querySelector('.next-btn');
+const random_btn = document.querySelector('random-btn');
+
+
+
+
+
 prev_btn.addEventListener('click', () => {
+
+})
+
+next_btn.addEventListener('click', () => {
+
+})
+
+random_btn.addEventListener('click', () => {
 
 })
